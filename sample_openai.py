1021bc2bc2e2269029ccabf02ba3ad12fa00e8df
Laurent Mazuel <laurent.mazuel@gmail.com>
2023-11-10 21:52:24 +0000

    Add UserAgent sniff

--- sample_openai.py
+++ sample_openai.py
@@ -5,8 +5,13 @@ import os
 from openai import OpenAIClient
 from openai.models import CreateChatCompletionResponse
 from corehttp.credentials import ServiceKeyCredential
+from corehttp.runtime.policies import SansIOHTTPPolicy
 
 api_key = os.getenv("OPENAI_API_KEY")
+
+#
+# A successfull call
+#
 
 client = OpenAIClient(ServiceKeyCredential(api_key))
 
@@ -23,6 +28,20 @@ completion: CreateChatCompletionResponse = client.create_chat_completion(
     }
 )
 print(completion.choices[0].message.content)
+
+#
+# Doing a call that fails, to show the stacktrace is Azure free.
+# Use this opportunity to show the UserAgent sent at the same time
+#
+
+class UserAgentExtractPolicy(SansIOHTTPPolicy):
+    def on_response(self, _, response) -> None:
+        print(f"UserAgent was: {response.http_request.headers['user-agent']}\n")
+
+client = OpenAIClient(
+    ServiceKeyCredential(api_key),
+    per_call_policies=UserAgentExtractPolicy(),
+)
 
 print("\n----- failed request -----")
 completion = client.create_chat_completion(
